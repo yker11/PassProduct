f6e2a76e1b08462dd51e0317f2c7728579366e0d
Julio Enrique Rojas Ramos <98976732+yker11@users.noreply.github.com>
2022-02-09 17:50:46 -0500

    Initial commit

---
+++ src/main/java/com/example/PassProduct/PassProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.PassProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PassProduct/PassProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PassProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
